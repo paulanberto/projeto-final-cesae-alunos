e1b95311358136fee313118214854c7d1991ba4b
guilherme-carasek <guilhermemcarasek@outlook.com>
2025-03-07 17:34:07 +0000

    forumCards

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ForumController;
 use App\Http\Controllers\DesignController;
 
 Route::get('/', function () {
@@ -11,3 +12,12 @@ Route::get('/', function () {
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 Route::get('/design', [DesignController::class, 'index'])->name('design');
+
+
+Route::prefix('forum')->group(function() {
+
+    Route::get('/', [ForumController::class, 'index'])->name('forum.index');
+
+    /* Route::get('/{id}') */
+
+});
